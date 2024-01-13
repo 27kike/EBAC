398aff27df8a1b2323561e26266a18a530e3b757
27kike <88414138+27kike@users.noreply.github.com>
2024-01-12 18:08:27 -0600

    Delete hellodjango directory

--- hellodjango/src/ecommerce/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class ProductModel(models.Model):
-    title = models.TextField()
-    color = models.TextField()
-    seller = models.TextField()
-    description = models.TextField()
-    price = models.FloatField()
